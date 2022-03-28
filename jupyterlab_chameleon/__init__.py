e71f3c36b531260a0e015371f4a8988a01673e2e
Jason Anderson <jasonanderson@uchicago.edu>
2022-03-29 10:16:23 +1100

    simplify request prep
    we don't really need to put the prepared request info behind an
    additional key, just moving this around while fixing how trovi
    is configured/called from the lab ext.

--- jupyterlab_chameleon/__init__.py
+++ jupyterlab_chameleon/__init__.py
@@ -20,16 +20,11 @@ with (HERE / "labextension" / "package.json").open() as fid:
 
 
 def _jupyter_labextension_paths():
-    return [{
-        "src": "labextension",
-        "dest": data["name"]
-    }]
+    return [{"src": "labextension", "dest": data["name"]}]
 
 
 def _jupyter_server_extension_points():
-    return [{
-        "module": "jupyterlab_chameleon"
-    }]
+    return [{"module": "jupyterlab_chameleon"}]
 
 
 def _load_jupyter_server_extension(server_app: "NotebookApp"):
@@ -42,22 +37,24 @@ def _load_jupyter_server_extension(server_app: "NotebookApp"):
     notebook_dir = server_app.notebook_dir or "."
 
     # Prepend the base_url so that it works in a jupyterhub setting
-    base_url = web_app.settings['base_url']
-    base_endpoint = url_path_join(base_url, 'chameleon')
-    contents_endpoint = url_path_join(base_endpoint, 'contents')
-    artifact_endpoint = url_path_join(base_endpoint, 'artifacts')
-    heartbeat_endpoint = url_path_join(base_endpoint, 'heartbeat')
+    base_url = web_app.settings["base_url"]
+    base_endpoint = url_path_join(base_url, "chameleon")
+    contents_endpoint = url_path_join(base_endpoint, "contents")
+    artifact_endpoint = url_path_join(base_endpoint, "artifacts")
+    heartbeat_endpoint = url_path_join(base_endpoint, "heartbeat")
 
-    db = DB(database=f'{notebook_dir}/.chameleon/chameleon.db')
+    db = DB(database=f"{notebook_dir}/.chameleon/chameleon.db")
 
     handlers = [
-        (contents_endpoint, ArtifactContentsHandler,
-            {'db': db, 'notebook_dir': notebook_dir}),
+        (
+            contents_endpoint,
+            ArtifactContentsHandler,
+            {"db": db, "notebook_dir": notebook_dir},
+        ),
         (heartbeat_endpoint, HeartbeatHandler),
-        (artifact_endpoint, ArtifactMetadataHandler,
-         {"db": db, "notebook_dir": notebook_dir}),
+        (artifact_endpoint, ArtifactMetadataHandler),
     ]
-    web_app.add_handlers('.*$', handlers)
+    web_app.add_handlers(".*$", handlers)
 
     init_db(server_app, db)
     server_app.log.info("Registered Chameleon extension at URL path /chameleon")
@@ -71,17 +68,20 @@ def init_db(server_app: "NotebookApp", db: "DB"):
     try:
         db.build_schema()
         # Also check if there is an initial artifact on the environment.
-        artifact_id = os.getenv('ARTIFACT_ID')
+        artifact_id = os.getenv("ARTIFACT_ID")
         if artifact_id:
             # Clear any existing artifacts; this is an ephemeral artifact
             # environment and it is OK to clean up for sanity. We can't do
             # this in a "workbench" server because the user may have multiple
             # artifacts linked to their working directory persisted.
             db.reset()
-            db.insert_artifact(Artifact(
-                path='', id=artifact_id,
-                deposition_repo=os.getenv('ARTIFACT_DEPOSITION_REPO'),
-                ownership=os.getenv('ARTIFACT_OWNERSHIP'),
-            ))
+            db.insert_artifact(
+                Artifact(
+                    path="",
+                    id=artifact_id,
+                    deposition_repo=os.getenv("ARTIFACT_DEPOSITION_REPO"),
+                    ownership=os.getenv("ARTIFACT_OWNERSHIP"),
+                )
+            )
     except Exception:
-        server_app.log.exception('Error initializing database')
+        server_app.log.exception("Error initializing database")
